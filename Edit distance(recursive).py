c9a4a87304cc6f6aabf2569e2444b572d9170318
tharun balanagu <tharunallzwell@gmail.com>
2020-10-07 15:11:55 +0530

    init

--- Edit distance(recursive).py
+++ Edit distance(recursive).py
@@ -1,13 +1,16 @@
-def oper(str1,str2,m,n):
-    if(m==0):
-        return n
-    if(n==0):
-        return m
-    if(str1[m-1]==str2[n-1]):
-        return oper(str1,str2,m-1,n-1)
-    else:
-        return 1+min(oper(str1,str2,m-1,n),oper(str1,str2,m,n-1),oper(str1,str2,m-1,n-1))
+
 for _ in range(int(input())):
     str1=input()
     str2=input()
-    print(oper(str1,str2,len(str1),len(str2)))
+    d=[[-1]*(len(str1)+1) for _ in range(len(str2)+1)]
+    for i in range(len(str1)+1):
+        d[0][i]=i
+        d[i][0]=i
+    
+    for i in range(1,len(str2)+1):
+        for j in range(1,len(str1)+1):
+            if(str2[i-1]==str1[j-1]):
+                d[i][j]=d[i-1][j-1]
+            else:
+                d[i][j]=1+min(d[i][j-1],d[i][j-1],d[i-1][j-1])
+    print(d[len(str2)][-1])
