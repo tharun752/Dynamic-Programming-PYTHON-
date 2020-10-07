c2f2fe82e4497a0c77f29ad419e149bca70533be
tharun balanagu <tharunallzwell@gmail.com>
2020-10-07 15:52:57 +0530

    init

--- coin change min number(DP).py
+++ coin change min number(DP).py
@@ -1,11 +1,20 @@
 def minimumNumberOfCoins(coins,numberOfCoins,value):
-    a=[[-1]*(value+1) for _ in range(numberOfCoins)]
+    a=[[-1]*(numberOfCoins+1) for _ in range(value+1)]
     for i in range(numberOfCoins):
-        a[i][0]=0
+        a[0][i]=0
+    for i in range(1,value+1):
+        a[i][0]=999
+        
     coins.sort()
-    for i in range(numberOfCoins):
-        for j in range(values+1):
-            if(arr[i]>j):
+    for i in range(1,value+1):
+        for j in range(1,numberOfCoins+1):
+            temp1=a[i][j-1]
+            temp2=999
+            if(i>=coins[j-1]):
+                temp2=a[i-coins[j-1]][j]
+            a[i][j]=min(temp1,temp2+1)
+    print(a[-1][-1])
+
                 
 
     
